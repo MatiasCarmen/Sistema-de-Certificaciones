d90c5b8b00612ed4422c09c9b0ebfd8c36924cf2
MatiasCarmen <U22209935@utp.edu.pe>
2026-03-05 13:34:29 -0500

    Agregar archivos de proyecto.

---
+++ Inkillay.Certificados.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inkillay.Certificados.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
